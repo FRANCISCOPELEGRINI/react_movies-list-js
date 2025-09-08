3059eb27894f4fd48992f9fba60db9d7190b1294
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-09-08 15:38:08 -0300

    sadsadasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,1 @@
-/* eslint-disable max-len */
-
-import './App.scss';
-import moviesFromServer from './api/movies.json';
-import { MovieList } from './components/MovieList/MovieList';
-
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <MovieList movies={moviesFromServer} />
-    </div>
-  </div>
-);
+d
