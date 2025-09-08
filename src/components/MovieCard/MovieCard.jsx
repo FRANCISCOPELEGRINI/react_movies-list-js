2fa1ef4469c36b4582b627e0668fbd3618179530
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-09-08 15:27:09 -0300

    Mate Academy React.JSX

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,6 +1,6 @@
 import './MovieCard.scss';
 
-export const MovieCard = ({ movie }) => (
+const MovieCard = ({ movie }) => (
   <div className="card" data-cy="Movie">
     <div className="card-image">
       <figure className="image is-4by3">
@@ -36,3 +36,5 @@ export const MovieCard = ({ movie }) => (
     </div>
   </div>
 );
+
+export default MovieCard;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,12 @@
-import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
+import MovieCard from '../MovieCard/MovieCard';
 
-export const MovieList = ({ movies }) => (
+const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
       <MovieCard movie={movie} key={movie.imdbId} />
     ))}
   </div>
 );
+
+export default MovieList;
